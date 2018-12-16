9b05a377fadadacdd442b7b04ab6fb9db6452681
Iury Miguel <iurymiguel@MacBook-Pro-do-Iury-Miguel.local>
2018-12-16 17:32:19 -0200

    Setting location permissions

--- app/src/main/java/com/example/iurymiguel/bluetoothapp/views/MainActivity.kt
+++ app/src/main/java/com/example/iurymiguel/bluetoothapp/views/MainActivity.kt
@@ -1,13 +1,106 @@
 package com.example.iurymiguel.bluetoothapp.views
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.widget.Toast
 import com.example.iurymiguel.bluetoothapp.R
+import com.example.iurymiguel.bluetoothapp.providers.BluetoothManagementProvider
+import org.koin.android.ext.android.inject
 
 class MainActivity : AppCompatActivity() {
+
+    private var mLocationPermission: Int = 0
+    private val MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1
+    private val TIME_TO_DISMISS_TOAST: Long = 4000
+    private val mBluetoothProvider: BluetoothManagementProvider by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askForLocationPermission()
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        if (mLocationPermission == PackageManager.PERMISSION_GRANTED) {
+            askForEnablingLocation()
+        }
+        enableBluetooth()
+    }
+
+
+    private fun askForLocationPermission() {
+        mLocationPermission = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+        if (mLocationPermission != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            ) {
+                showWarning()
+            } else {
+                requestPermission()
+            }
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS -> {
+                if (grantResults.isNotEmpty() &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+                ) {
+                    askForEnablingLocation()
+                }
+            }
+        }
+    }
+
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION),
+            MY_PERMISSIONS_REQUEST_READ_CONTACTS
+        )
+    }
+
+
+    private fun showWarning() {
+        Toast.makeText(this, getString(R.string.enable_location_warning), Toast.LENGTH_LONG).show()
+        Handler().postDelayed(object : Runnable {
+            override fun run() {
+                requestPermission()
+            }
+        }, TIME_TO_DISMISS_TOAST)
+    }
+
+    private fun askForEnablingLocation() {
+
+    }
+
+
+    /**
+     * Enables bluetooth if disabled.
+     */
+    private fun enableBluetooth() {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if(!bluetoothAdapter.isEnabled) {
+            bluetoothAdapter.enable()
+        }
+    }
+
+
 }
